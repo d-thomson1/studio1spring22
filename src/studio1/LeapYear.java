1f43e5df65ea1f39cb2392645f1042bebb8cf974
dell <dell@192.168.1.151>
2022-01-27 14:50:43 -0600

    LeapYear

--- src/studio1/LeapYear.java
+++ src/studio1/LeapYear.java
@@ -6,6 +6,7 @@ public class LeapYear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		ArgsProcessor ap = new ArgsProcessor (args);
 		int year = ap.nextInt("the year");
 		System.out.println(year%400==0||(year%4==0&&year%100!=0));
